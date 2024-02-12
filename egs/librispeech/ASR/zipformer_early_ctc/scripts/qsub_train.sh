ea8fd74e9cd325a573d3e54cfe0e1c7049fa3bf5
Ruizhe (Ray) Huang <eraser567@163.com>
2024-02-12 17:03:18 -0500

    updates: zipformer_early_ctc

--- egs/librispeech/ASR/zipformer_early_ctc/scripts/qsub_train.sh
+++ egs/librispeech/ASR/zipformer_early_ctc/scripts/qsub_train.sh
@@ -18,7 +18,7 @@
 # hostname
 # nvidia-smi
 
-# conda activate aligner5
+# conda activate /home/hltcoe/rhuang/mambaforge/envs/aligner5
 export PATH="/home/hltcoe/rhuang/mambaforge/envs/aligner5/bin/":$PATH
 module load cuda11.7/toolkit
 module load cudnn/8.5.0.96_cuda11.x
@@ -77,6 +77,57 @@ python zipformer_early_ctc/train.py \
   --exp-dir $exp_dir \
   --max-duration 1000
 
+# export CUDA_VISIBLE_DEVICES="0"
+# for m in ctc-decoding 1best nbest nbest-rescoring whole-lattice-rescoring; do
+#   ./zipformer_early_ctc/ctc_decode.py \
+#       --epoch 40 \
+#       --avg 16 \
+#       --exp-dir $exp_dir \
+#       --use-transducer 1 \
+#       --use-ctc 1 \
+#       --max-duration 300 \
+#       --causal 0 \
+#       --num-paths 100 \
+#       --nbest-scale 1.0 \
+#       --hlg-scale 0.6 \
+#       --decoding-method $m
+# done
+
+######## exp_dir=zipformer_early_ctc/exp/exp-test-4 ########
+# ctc-decoding 4.22/10.02
+# 1best 3.24/7.39
+# nbest 3.24/7.39
+# nbest-rescoring  2.96/6.86
+# whole-lattice-rescoring 2.95/6.83
+######## exp_dir=zipformer_early_ctc/exp/exp-test-5 ########
+# ctc-decoding 6.43/14.58
+# 1best 4.33/9.98
+# nbest 4.33/9.99
+# nbest-rescoring  3.86/9.1
+# whole-lattice-rescoring 3.85/8.94
+
+
+# export CUDA_VISIBLE_DEVICES="1"
+# for m in greedy_search modified_beam_search fast_beam_search; do
+#   ./zipformer_early_ctc/decode.py \
+#     --epoch 40 \
+#     --avg 16 \
+#     --use-averaged-model 1 \
+#     --use-transducer 1 \
+#     --use-ctc 1 \
+#     --exp-dir $exp_dir \
+#     --max-duration 600 \
+#     --decoding-method $m
+# done
+
+######## exp_dir=zipformer_early_ctc/exp/exp-test-4 ########
+# greedy_search 2.17/4.93
+# modified_beam_search 2.13/4.87
+# fast_beam_search 2.14/4.86
+######## exp_dir=zipformer_early_ctc/exp/exp-test-5 ########
+# greedy_search 2.25/5.14
+# modified_beam_search 2.22/5.08
+# fast_beam_search 2.25/5.07
 
 ####################################
 # train ctc
@@ -127,3 +178,26 @@ python posterior_dropout/train.py \
 ####################################
 # tensorboard dev upload --logdir /exp/rhuang/meta/icefall/egs/librispeech/ASR/$exp_dir/tensorboard --description `pwd`
 # tensorboard dev upload --logdir $exp_dir/tensorboard --description `pwd`/$exp_dir/
+
+# # https://www.xmodulo.com/skip-existing-files-scp.html
+# scp -r \
+#   rhuang25@login.rockfish.jhu.edu:/scratch4/skhudan1/rhuang25/icefall/egs/librispeech/ASR/zipformer_early_ctc/exp/exp-test-4/tensorboard \
+#   zipformer_early_ctc/exp/exp-test/tensorboard/exp-test-4
+# scp -r \
+#   rhuang25@login.rockfish.jhu.edu:/scratch4/skhudan1/rhuang25/icefall/egs/librispeech/ASR/zipformer_early_ctc/exp/exp-test-5/tensorboard \
+#   zipformer_early_ctc/exp/exp-test/tensorboard/exp-test-5
+# rsync -avz --progress zipformer_early_ctc/exp/exp-test/tensorboard/exp-test-4 rhuang25@login.rockfish.jhu.edu:/scratch4/skhudan1/rhuang25/icefall/egs/librispeech/ASR/zipformer_early_ctc/exp/exp-test-4/tensorboard
+# rsync -avz --progress zipformer_early_ctc/exp/exp-test/tensorboard/exp-test-5 rhuang25@login.rockfish.jhu.edu:/scratch4/skhudan1/rhuang25/icefall/egs/librispeech/ASR/zipformer_early_ctc/exp/exp-test-5/tensorboard
+# scp \
+#   rhuang25@login.rockfish.jhu.edu:/scratch4/skhudan1/rhuang25/icefall/egs/librispeech/ASR/zipformer_early_ctc/exp/exp-test-4/tensorboard/* \
+#   zipformer_early_ctc/exp/exp-test/tensorboard/exp-test-4/.
+# scp \
+#   rhuang25@login.rockfish.jhu.edu:/scratch4/skhudan1/rhuang25/icefall/egs/librispeech/ASR/zipformer_early_ctc/exp/exp-test-5/tensorboard/* \
+#   zipformer_early_ctc/exp/exp-test/tensorboard/exp-test-5/.
+
+
+# source activate aligner5
+# tensorboard --logdir $exp_dir/tensorboard --port 6006 --window_title $exp_dir
+# ssh -L 16006:127.0.0.1:6006 rhuang25@login.rockfish.jhu.edu
+# ssh -L 16006:127.0.0.1:6006 rhuang@test2.hltcoe.jhu.edu
+# http://localhost:16006 
